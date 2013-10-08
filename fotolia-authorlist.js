b2bc3b10b08bac524a8b001cbfe26fc0276b85b1
Felix Ebert <felix.ebert@if-core.de>
2013-10-08 22:24:21 +0200

    fotolia-image.js

--- fotolia-authorlist.js
+++ fotolia-authorlist.js
@@ -1,36 +1,20 @@
-var fs = require('fs'), path = require('path'), ExifImage = require('exif').ExifImage;
+var fs = require('fs'), path = require('path'), ExifImage = require('exif').ExifImage, FotoliaImage = require('./lib/fotolia-image.js');
+var argv = require('optimist').usage('List the authors of all fotolia images in a directory\nUsage: $0 [directory]').demand(1).argv;
 
-if (process.argv.length < 3) {
-	process.exit(1);
-}
-var imagePath = process.argv[2];
-var imageFiles = fs.readdirSync(imagePath);
+var imagePath = argv._[0] + path.sep;
 
-var outputAuthor = function(imageFile, exifData) {
-	if (exifData && exifData.image && exifData.image.Copyright && exifData.image.Copyright.trim()) {
-		var author = '© ' + exifData.image.Copyright.replace(/\(C\)\ /g, '');
-		if (imageFile.toLowerCase().indexOf('fotolia') > -1 && author.toLowerCase().indexOf('fotolia') < 0) {
-			author += ' / Fotolia.com';
-		}
-		console.log(author);
-	}
-};
-
-imageFiles.forEach(function(imageFile) {
-	if (path.extname(imageFile) === '.jpg' || path.extname(imageFile) === '.jpeg') {
-		fs.stat(imagePath + imageFile, function(fsError, stats) {
+fs.readdirSync(imagePath).forEach(function(file) {
+	if (path.extname(file) === '.jpg' || path.extname(file) === '.jpeg') {
+		fs.stat(imagePath + file, function(fsError, stats) {
 			if (fsError) {
 				throw new Error(fsError);
 			}
 
 			if (stats.isFile()) {
-				new ExifImage({
-					'image': imagePath + imageFile
-				}, function(exifError, exifData) {
-					if (exifError) {
-						console.error(exifError);
-					} else {
-						outputAuthor(imageFile, exifData);
+				var image = new FotoliaImage(imagePath + file);
+				image.getAuthor(function(author) {
+					if (author != null) {
+						console.log(author);
 					}
 				});
 			}
